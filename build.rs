085011cd22f603a42529645a05d5d426d3b85ad5
colbyn <hello@colbyn.com>
2019-12-09 12:44:41 -0700

    issues with avfilter

--- build.rs
+++ build.rs
@@ -179,15 +179,15 @@ pub const HEADER_GROUPS: &[(&str, &[&str])] = &[
             "libavdevice/avdevice.h",
         ]
     ),
-    (
-        "avfilter",
-        &[
-            // // DEPS
-            // "libavfilter/version.h",
-            // EXTERNAL
-            "libavfilter/avfilter.h",
-        ]
-    ),
+    // (
+    //     "avfilter",
+    //     &[
+    //         // // DEPS
+    //         // "libavfilter/version.h",
+    //         // EXTERNAL
+    //         "libavfilter/avfilter.h",
+    //     ]
+    // ),
     (
         "avformat",
         &[
